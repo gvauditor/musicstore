61a2433544028510c4860747fcbc5850f7fd3428
gvauditor <geovanni.auditor@gmail.com>
2016-07-29 17:15:41 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Music Store</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="css/custom.css">
+</head>
+<body>
+  
+<div class="container">
+	<div class="row">
+	<header> <a href="index.php">My Tunes</a> </header>
+	<nav class="pdsa-sn-wrapper">
+		<ul>
+			<li class="pds-sn-brand">
+				<a href="index.php">Music</a>
+			</li>
+			<li>
+				<a href="#">Home</a>
+			</li>
+			<li>
+				<a href="#">Genres</a>
+			</li>
+			<li>
+				<a href="#">Playlists</a>
+			</li>
+			<li>
+				<a href="#">Songs</a>
+			</li>
+			<li>
+				<a href="#">Downloads</a>
+			</li>
+		</ul>
+	</nav>
+	</div>
+</div>
+
+<div class="container body-content">
+	<div class="row">
+		<div class="col-xs-10 col-lg-12">
+			<h1>My Music</h1>
+			<p>This is where you gather all of your music
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
